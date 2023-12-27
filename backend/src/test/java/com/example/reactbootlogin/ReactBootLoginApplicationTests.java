134e406cd33b012ca87e62804696210552dc229e
junwon_lee <wjdrbs307@naver.com>
2023-12-27 15:47:06 +0900

    백엔드 프론트 셋팅다시

---
+++ backend/src/test/java/com/example/reactbootlogin/ReactBootLoginApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.reactbootlogin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReactBootLoginApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
